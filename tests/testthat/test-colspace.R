dc396860ce225618fec933618bced4a5cdb84930
Hugo Gruson <hugo.gruson@protonmail.com>
2020-02-05 13:37:56 +0100

    Split tests into multiple files

---
+++ tests/testthat/test-colspace.R
@@ -0,0 +1,49 @@
+context("colspace")
+
+data(sicalis)
+
+test_that("tcs", {
+
+  tcs_sicalis <- colspace(vismodel(sicalis, relative = TRUE))
+
+  expect_doppelganger("tetraplot", plot(tcs_sicalis, col = spec2rgb(sicalis)))
+  expect_identical(plot(tcs_sicalis), tetraplot(tcs_sicalis))
+
+  skip_if_not_installed("mapproj")
+
+  expect_doppelganger("projplot", projplot(tcs_sicalis))
+
+})
+
+test_that("tri", {
+
+  tri_sicalis <- colspace(vismodel(sicalis, visual = "apis"))
+
+  expect_doppelganger("triplot", plot(tri_sicalis, col = spec2rgb(sicalis)))
+  expect_identical(plot(tri_sicalis), triplot(tri_sicalis))
+
+})
+
+test_that("di", {
+
+  di_sicalis <- colspace(vismodel(sicalis, visual = "canis"))
+
+  expect_doppelganger("diplot", plot(di_sicalis, col = spec2rgb(sicalis)))
+  expect_identical(plot(di_sicalis), diplot(di_sicalis))
+
+})
+
+test_that("cie", {
+
+  ciexyz_sicalis <- colspace(vismodel(sicalis, visual = "cie10"))
+  cielch_sicalis <- colspace(vismodel(sicalis, visual = "cie10"), space = "cielch")
+  cielab_sicalis <- colspace(vismodel(sicalis, visual = "cie10"), space = "cielab")
+
+  expect_doppelganger("ciexyz", plot(ciexyz_sicalis))
+  expect_doppelganger("cielch", plot(cielch_sicalis))
+  expect_doppelganger("cielab", plot(cielab_sicalis))
+
+  expect_identical(plot(ciexyz_sicalis), cieplot(ciexyz_sicalis))
+
+})
+
---
+++ tests/testthat/test-rspec.R
@@ -0,0 +1,40 @@
+context("rspec")
+data(sicalis)
+data(teal)
+
+test_that("plot.rspec", {
+
+  expect_doppelganger("plot.rspec-overlay", plot(sicalis, col = spec2rgb(sicalis)))
+  expect_identical({plot(sicalis, type = "overlay")}, {plot(sicalis)})
+  expect_doppelganger("plot.rspec-stack", plot(sicalis, type = "stack"))
+  expect_doppelganger("plot.rspec-select", plot(sicalis, select = 2))
+  expect_doppelganger("plot.rspec-heatmap", plot(sicalis, type = "heatmap"))
+
+})
+
+test_that("explorespec", {
+
+  expect_doppelganger("explorespec", {par(mfrow = c(3,3)); explorespec(sicalis[,1:10])})
+
+})
+
+test_that("aggplot", {
+
+  expect_doppelganger("aggplot", aggplot(sicalis, by = 3))
+  expect_doppelganger("aggplot-quantile", aggplot(sicalis, by = 3, FUN.error = function(x) quantile(x, c(0.0275, 0.975))) )
+  expect_doppelganger("aggplot-shadecol", aggplot(sicalis, by = 3, shadecol = spec2rgb(sicalis), lcol = 1))
+
+})
+
+test_that("plotsmooth", {
+
+  expect_doppelganger("plotsmooth", plotsmooth(sicalis[1:7], ask = FALSE))
+
+})
+
+test_that("peakshape", {
+
+  expect_doppelganger("peakshape", {par(mfrow = c(3,3)); peakshape(sicalis[,1:10])})
+  expect_doppelganger("peakshape-lims", {par(mfrow = c(3,3)); peakshape(sicalis[,1:10], lim = c(300,400))})
+
+})
